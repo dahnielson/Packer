7873d64245e99f3831b45dbc5d950015f03bc7bf
Anders Dahnielson <anders@dahnielson.com>
2024-06-19 19:38:28 +0200

    Add files

---
+++ Packer/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Packer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            redBox = new PictureBox();
+            greenBox = new PictureBox();
+            blueBox = new PictureBox();
+            alphaBox = new PictureBox();
+            packedBox = new PictureBox();
+            saveButton = new Button();
+            clearButton = new Button();
+            quitButton = new Button();
+            saveFileDialog1 = new SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)redBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)greenBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)blueBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)alphaBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)packedBox).BeginInit();
+            SuspendLayout();
+            // 
+            // redBox
+            // 
+            redBox.BackgroundImage = Properties.Resources.red;
+            redBox.BackgroundImageLayout = ImageLayout.Zoom;
+            redBox.BorderStyle = BorderStyle.FixedSingle;
+            redBox.Location = new Point(12, 12);
+            redBox.Name = "redBox";
+            redBox.Size = new Size(128, 128);
+            redBox.SizeMode = PictureBoxSizeMode.Zoom;
+            redBox.TabIndex = 0;
+            redBox.TabStop = false;
+            redBox.DragDrop += redBox_DragDrop;
+            redBox.DragEnter += redBox_DragEnter;
+            // 
+            // greenBox
+            // 
+            greenBox.BackgroundImage = Properties.Resources.green;
+            greenBox.BackgroundImageLayout = ImageLayout.Zoom;
+            greenBox.BorderStyle = BorderStyle.FixedSingle;
+            greenBox.Location = new Point(12, 146);
+            greenBox.Name = "greenBox";
+            greenBox.Size = new Size(128, 128);
+            greenBox.SizeMode = PictureBoxSizeMode.Zoom;
+            greenBox.TabIndex = 1;
+            greenBox.TabStop = false;
+            greenBox.DragDrop += greenBox_DragDrop;
+            greenBox.DragEnter += greenBox_DragEnter;
+            // 
+            // blueBox
+            // 
+            blueBox.BackgroundImage = Properties.Resources.blue;
+            blueBox.BackgroundImageLayout = ImageLayout.Zoom;
+            blueBox.BorderStyle = BorderStyle.FixedSingle;
+            blueBox.Location = new Point(12, 280);
+            blueBox.Name = "blueBox";
+            blueBox.Size = new Size(128, 128);
+            blueBox.SizeMode = PictureBoxSizeMode.Zoom;
+            blueBox.TabIndex = 2;
+            blueBox.TabStop = false;
+            blueBox.DragDrop += blueBox_DragDrop;
+            blueBox.DragEnter += blueBox_DragEnter;
+            // 
+            // alphaBox
+            // 
+            alphaBox.BackgroundImage = Properties.Resources.alpha;
+            alphaBox.BackgroundImageLayout = ImageLayout.Zoom;
+            alphaBox.BorderStyle = BorderStyle.FixedSingle;
+            alphaBox.Location = new Point(12, 414);
+            alphaBox.Name = "alphaBox";
+            alphaBox.Size = new Size(128, 128);
+            alphaBox.SizeMode = PictureBoxSizeMode.Zoom;
+            alphaBox.TabIndex = 3;
+            alphaBox.TabStop = false;
+            alphaBox.DragDrop += alphaBox_DragDrop;
+            alphaBox.DragEnter += alphaBox_DragEnter;
+            // 
+            // packedBox
+            // 
+            packedBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            packedBox.BackgroundImage = Properties.Resources.checkerboard;
+            packedBox.BackgroundImageLayout = ImageLayout.Zoom;
+            packedBox.BorderStyle = BorderStyle.FixedSingle;
+            packedBox.Location = new Point(146, 12);
+            packedBox.Name = "packedBox";
+            packedBox.Size = new Size(530, 530);
+            packedBox.SizeMode = PictureBoxSizeMode.Zoom;
+            packedBox.TabIndex = 2;
+            packedBox.TabStop = false;
+            // 
+            // saveButton
+            // 
+            saveButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            saveButton.Location = new Point(520, 548);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new Size(75, 23);
+            saveButton.TabIndex = 4;
+            saveButton.Text = "Save As...";
+            saveButton.UseVisualStyleBackColor = true;
+            saveButton.Click += saveButton_Click;
+            // 
+            // clearButton
+            // 
+            clearButton.Location = new Point(12, 548);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(75, 23);
+            clearButton.TabIndex = 5;
+            clearButton.Text = "Clear";
+            clearButton.UseVisualStyleBackColor = true;
+            clearButton.Click += clearButton_Click;
+            // 
+            // quitButton
+            // 
+            quitButton.Location = new Point(601, 548);
+            quitButton.Name = "quitButton";
+            quitButton.Size = new Size(75, 23);
+            quitButton.TabIndex = 6;
+            quitButton.Text = "Quit";
+            quitButton.UseVisualStyleBackColor = true;
+            quitButton.Click += quitButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(688, 583);
+            Controls.Add(quitButton);
+            Controls.Add(clearButton);
+            Controls.Add(saveButton);
+            Controls.Add(redBox);
+            Controls.Add(packedBox);
+            Controls.Add(blueBox);
+            Controls.Add(alphaBox);
+            Controls.Add(greenBox);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Packer";
+            ((System.ComponentModel.ISupportInitialize)redBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)greenBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)blueBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)alphaBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)packedBox).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox redBox;
+        private PictureBox greenBox;
+        private PictureBox blueBox;
+        private PictureBox alphaBox;
+        private PictureBox packedBox;
+        private Button saveButton;
+        private Button clearButton;
+        private Button quitButton;
+        private SaveFileDialog saveFileDialog1;
+    }
+}
---
+++ Packer/Form1.cs
@@ -0,0 +1,266 @@
+using System.Diagnostics;
+using System.Drawing.Imaging;
+
+namespace Packer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            redBox.AllowDrop = true; // R
+            greenBox.AllowDrop = true; // G
+            blueBox.AllowDrop = true; // B
+            alphaBox.AllowDrop = true; // A
+            packedBox.AllowDrop = false; // RGBA
+        }
+
+        private void redBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data == null)
+                return;
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void redBox_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data == null)
+                return;
+
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            if (files != null && files.Any())
+            {
+                redBox.Image = new Bitmap(files[0]);
+                packChannels();
+            }
+        }
+
+        private void greenBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data == null)
+                return;
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void greenBox_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data == null)
+                return;
+
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            if (files != null && files.Any())
+            {
+                greenBox.Image = new Bitmap(files[0]);
+                packChannels();
+            }
+        }
+
+        private void blueBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data == null)
+                return;
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void blueBox_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data == null)
+                return;
+
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            if (files != null && files.Any())
+            {
+                blueBox.Image = new Bitmap(files[0]);
+                packChannels();
+            }
+        }
+
+        private void alphaBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data == null)
+                return;
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void alphaBox_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data == null)
+                return;
+
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            if (files != null && files.Any())
+            {
+                alphaBox.Image = new Bitmap(files[0]);
+                packChannels();
+            }
+        }
+
+        private void packChannels()
+        {
+            Bitmap redChannel = null;
+            Bitmap greenChannel = null;
+            Bitmap blueChannel = null;
+            Bitmap alphaChannel = null;
+
+            int width = 0;
+            int height = 0;
+
+            if (redBox.Image != null)
+            {
+                if (redBox.Image.Width > width)
+                    width = redBox.Image.Width;
+                if (redBox.Image.Height > height)
+                    height = redBox.Image.Height;
+                redChannel = new Bitmap(redBox.Image);
+            }
+
+            if (greenBox.Image != null)
+            {
+                if (greenBox.Image.Width > width)
+                    width = greenBox.Image.Width;
+                if (greenBox.Image.Height > height)
+                    height = greenBox.Image.Height;
+                greenChannel = new Bitmap(greenBox.Image);
+            }
+
+            if (blueBox.Image != null)
+            {
+                if (blueBox.Image.Width > width)
+                    width = blueBox.Image.Width;
+                if (blueBox.Image.Height > height)
+                    height = blueBox.Image.Height;
+                blueChannel = new Bitmap(blueBox.Image);
+            }
+
+            if (alphaBox.Image != null)
+            {
+                if (alphaBox.Image.Width > width)
+                    width = alphaBox.Image.Width;
+                if (alphaBox.Image.Height > height)
+                    height = alphaBox.Image.Height;
+                alphaChannel = new Bitmap(alphaBox.Image);
+            }
+
+            if (width == 0 || height == 0)
+                return;
+
+            Bitmap packed = new Bitmap(width, height);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int r = 0;
+                    int g = 0;
+                    int b = 0;
+                    int a = 255;
+
+                    if (redChannel != null)
+                    {
+                        int sampleX = x;
+                        int sampleY = y;
+
+                        if (redChannel.Width < width)
+                            sampleX = (x / width) * redChannel.Width;
+                        if (redChannel.Height < height)
+                            sampleY = (y / height) * redChannel.Height;
+                        
+                        r = redChannel.GetPixel(sampleX, sampleY).R;
+                    }
+
+                    if (greenChannel != null)
+                    {
+                        int sampleX = x;
+                        int sampleY = y;
+
+                        if (greenChannel.Width < width)
+                            sampleX = (x / width) * greenChannel.Width;
+                        if (greenChannel.Height < height)
+                            sampleY = (y / height) * greenChannel.Height;
+
+                        g = greenChannel.GetPixel(sampleX, sampleY).R;
+                    }
+
+                    if (blueChannel != null)
+                    {
+                        int sampleX = x;
+                        int sampleY = y;
+
+                        if (blueChannel.Width < width)
+                            sampleX = (x / width) * blueChannel.Width;
+                        if (blueChannel.Height < height)
+                            sampleY = (y / height) * blueChannel.Height;
+
+                        b = blueChannel.GetPixel(sampleX, sampleY).R;
+                    }
+
+                    if (alphaChannel != null)
+                    {
+                        int sampleX = x;
+                        int sampleY = y;
+
+                        if (alphaChannel.Width < width)
+                            sampleX = (x / height) * alphaChannel.Width;
+                        if (alphaChannel.Height < height)
+                            sampleY = (y / height) * alphaChannel.Height;
+
+                        a = alphaChannel.GetPixel(x, y).R;
+                    }
+
+                    Color packedColor = Color.FromArgb(a, r, g, b);
+                    packed.SetPixel(x, y, packedColor);
+                }
+            }
+
+            packedBox.Image = packed;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            redBox.Image = null;
+            greenBox.Image = null;
+            blueBox.Image = null;
+            alphaBox.Image = null;
+            packedBox.Image = null;
+        }
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (packedBox.Image == null)
+            {
+                MessageBox.Show("No channel packed image to save.", "No Image", MessageBoxButtons.OK);
+                return;
+            }
+
+            saveFileDialog1.Filter = "PNG Image|*.png";
+            saveFileDialog1.Title = "Save Image File";
+            if (DialogResult.OK == saveFileDialog1.ShowDialog(this) && saveFileDialog1.FileName != "")
+                packedBox.Image.Save(saveFileDialog1.FileName, ImageFormat.Png);
+        }
+    }
+}
